fb5d1c4966d229d10d9d83adde41ee19eed2fcab
Rana <meghasselrana2004@gmail.com>
2025-08-20 13:48:56 +0100

    linked my part with oumnia code in a new testing branch

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,23 +4,26 @@ import Auth from "./components/Auth.jsx";
 import ResetPassword from "./components/ResetPassword.jsx";
 import ForgotPassword from "./components/ForgotPassword.jsx";
 import Login from "./components/Login.jsx";
+import Homepage from "./components/Homepage.jsx";
+
+
 
 function App() {
       return (
             <div className="App">
-                <Routes>
-                      {/* Redirect root to /login */}
-                      <Route path="/" element={<Navigate to="/login" />} />
+                    <Routes>
 
-                      {/* Auth routes */}
-                      <Route path="/login" element={<Auth />} />
-                      <Route path="/register" element={<Auth />} />
-                      <Route path="/forgot_password" element={<ForgotPassword />} />
-                      <Route path="/reset_password" element={<ResetPassword />} />
-                      <Route path="/login_admin" element={<Login/>}/>
+                          {/* Auth routes */}
+                          <Route path="/login" element={<Auth />} />
+                          <Route path="/register" element={<Auth />} />
+                          <Route path="/forgot_password" element={<ForgotPassword />} />
+                          <Route path="/reset_password" element={<ResetPassword />} />
+                          <Route path="/login_admin" element={<Login/> }/>
 
-                      <Route path="*" element={<h1>404 - Page Not Found</h1>} />
-                </Routes>
+                          <Route path="/" element={<Homepage />} />
+                          {/* Redirect unknown paths to home */}
+                          <Route path="*" element={<Navigate to="/" replace />} />
+                    </Routes>
             </div>
       );
 }
---
+++ frontend/src/components/Homepage.jsx
@@ -0,0 +1,23 @@
+import Header from "../components/Header";
+import Hero from "../components/Hero";
+import Card from "../components/Card";
+import Product from "../components/Product";
+import Footer from "../components/Footer";
+import Beneficiaries from "./Beneficiaries.jsx";
+import Donates from "./Donates.jsx";
+
+const HomePage = () => {
+    return (
+        <div className="bg-white w-full">
+            <Header />
+            <Hero />
+            <Card />
+            <Product />
+            <Donates />
+            <Beneficiaries />
+            <Footer />
+        </div>
+    );
+};
+
+export default HomePage;
