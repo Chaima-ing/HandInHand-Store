f98bc279e9188fa5732c9deee397e44e268de1a0
Rana <meghasselrana2004@gmail.com>
2025-08-20 16:41:14 +0100

    WIP: save changes before switching branch

--- Home-pages/src/App.jsx
+++ Home-pages/src/App.jsx
@@ -1,22 +1,34 @@
+import { Routes, Route, Navigate } from "react-router-dom";
 import './App.css';
-import Footer from './assets/Footer.jsx';
-import Header from './assets/Header.jsx';
-import Hero from './assets/Hero.jsx';
-import Product from './assets/Product.jsx';
-import Card from './assets/Card.jsx';
-import Donates from './assets/Donates.jsx';
-import Beneficiaries from './assets/Beneficiaries.jsx';
+import Footer from './components/Footer.jsx';
+import Header from './components/Header.jsx';
+import Hero from './components/Hero.jsx';
+import Product from './components/Product.jsx';
+import Card from './components/Card.jsx';
+import Donates from './components/Donates.jsx';
+import Beneficiaries from './components/Beneficiaries.jsx';
+
+import Auth from './components/Auth.jsx';
+
 function App() {
     return(
-      <>
-      <Header/>
-      <Hero/>
-      <Card/>
-      <Product/>
-      <Donates/>
-      <Beneficiaries/>
-      <Footer/>
-      </>
+        <>
+        <Header />
+          <Routes>
+              <Route path="/" element={<Hero />} />
+              <Route path="/card" element={<Card />} />
+              <Route path="/products" element={<Product />} />
+              <Route path="/donates" element={<Donates />} />
+              <Route path="/beneficiaries" element={<Beneficiaries />} />
+
+              <Route path="/login" element={<Auth />} />
+              <Route path="/register" element={<Auth />} />
+
+              {/* Redirect unknown paths to home */}
+              <Route path="*" element={<Navigate to="/" replace />} />
+          </Routes>
+        <Footer />
+        </>
     );
 }
 
