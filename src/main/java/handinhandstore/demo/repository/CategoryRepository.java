b8a54d5f552f827a07ba20abb1a063dad9a2ef09
chaima <chaimaa.djekhrab@gmail.com>
2025-09-02 12:16:16 +0100

    feat(manage-categories): implement category update by id & passing the whole category

--- src/main/java/handinhandstore/demo/repository/CategoryRepository.java
+++ src/main/java/handinhandstore/demo/repository/CategoryRepository.java
@@ -1,8 +1,16 @@
 package handinhandstore.demo.repository;
 
 import handinhandstore.demo.model.entity.Category;
+
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
     Category findByName(String name);
+
+   @Query("SELECT c FROM Category c WHERE c.id = :id")
+   Optional<Category>  findById(@Param("id") Long id);
 }
