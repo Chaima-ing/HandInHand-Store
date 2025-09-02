6abc812fe6327c439bb526b1129c0d371925e2c4
chaima <chaimaa.djekhrab@gmail.com>
2025-09-02 12:01:37 +0100

    fix(manage-category): add default constructor for category

--- src/main/java/handinhandstore/demo/model/entity/Category.java
+++ src/main/java/handinhandstore/demo/model/entity/Category.java
@@ -17,6 +17,10 @@ public class Category {
     @ManyToMany(mappedBy = "categories")
     private Set<Product> products = new HashSet<>();
 
+    public Category(){
+        
+    }
+
     public Category(String name, String icon){
         this.name = name;
         this.icon = icon;
