eb4a6c3182805b6759d4af7dc3b46dbda39c30ad
chaima <chaimaa.djekhrab@gmail.com>
2025-09-07 14:27:40 +0100

    feat(create-order):
     - create order necessary Reques&Response dto's
    
     - add order repository

---
+++ src/main/java/handinhandstore/demo/repository/OrderRepository.java
@@ -0,0 +1,10 @@
+package handinhandstore.demo.repository;
+
+import handinhandstore.demo.model.entity.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
+    
+}
